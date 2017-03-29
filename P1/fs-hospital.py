2760f0ffd4ed9dd42c899cdd245a3890a42d471c
Marc Juchli <mail@marcjuch.li>
2017-03-29 17:32:56 +0200

    Limit to NY

--- P1/fs-hospital.py
+++ P1/fs-hospital.py
@@ -27,40 +27,45 @@ def collectHospitalsFromCityFile(path):
             city = row[0]
             lat = convert(row[1])
             lon = convert(row[2])
-            print city + ": " + str(lat) + ", " +str(lon)
-            response = getHospitals(city, lat, lon)
-            code = response['meta']['code']
-            if code == 200:
-                filename = city+'.json'
-            else:
-                filename = 'error-'+city+'.json'
+            if 'N.Y.' in city:
+                print city + ": " + str(lat) + ", " +str(lon)
+                response = getHospitals(city, lat, lon)
+                code = response['meta']['code']
+                if code == 200:
+                    filename = city+'.json'
+                else:
+                    filename = 'error-'+city+'.json'
 
-            with open('collection/'+filename, 'w') as outfile:
-                json.dump(response, outfile)
+                with open('ny/'+filename, 'w') as outfile:
+                    json.dump(response, outfile)
 
 # 'cities-usa.tsv'
-#def findLargestRectangleFromCityFile(path):
-with open("cities-usa.tsv",'rb') as tsvin:
-    tsvin = csv.reader(tsvin, delimiter='\t')
-    minLat = 1000
-    minLon = 1000
-    maxLat = -1000
-    maxLon = -1000
-    for row in tsvin:
-        city = row[0]
-        lat = convert(row[1])
-        lon = convert(row[2])
-        if (lat > 17.0 and lat < 90.0 and lon > -190.0 and lon < -55.13): # rough boundary around the us incl. alaska and hawaii
-            print "\""+city+"\"" + ": { center: {lat: "+str(lat)+", lng: "+str(lon)+"} },"
-            minLat = min(minLat, lat)
-            minLon = min(minLon, lon)
-            maxLat = max(maxLat, lat)
-            maxLon = max(maxLon, lon)
-    print "south: " + str(minLat) + ","
-    print "north: " + str(maxLat) + ","
-    print "west: " + str(minLon) + ","
-    print "east: " + str(maxLon)
-    # north: 50,
-    # south:  17,
-    # west: -170.0,
-    # east: -55.1333333333
+def findLargestRectangleFromCityFile(path):
+    #with open("cities-usa.tsv",'rb') as tsvin:
+    with open(path,'rb') as tsvin:
+        tsvin = csv.reader(tsvin, delimiter='\t')
+        minLat = 1000
+        minLon = 1000
+        maxLat = -1000
+        maxLon = -1000
+        for row in tsvin:
+            city = row[0]
+            if 'N.Y.' in city:
+                lat = convert(row[1])
+                lon = convert(row[2])
+                if (lat > 17.0 and lat < 90.0 and lon > -190.0 and lon < -55.13): # rough boundary around the us incl. alaska and hawaii
+                    print "\""+city+"\"" + ": { center: {lat: "+str(lat)+", lng: "+str(lon)+"} },"
+                    minLat = min(minLat, lat)
+                    minLon = min(minLon, lon)
+                    maxLat = max(maxLat, lat)
+                    maxLon = max(maxLon, lon)
+        print "south: " + str(minLat) + ","
+        print "north: " + str(maxLat) + ","
+        print "west: " + str(minLon) + ","
+        print "east: " + str(maxLon)
+        # north: 50,
+        # south:  17,
+        # west: -170.0,
+        # east: -55.1333333333
+
+collectHospitalsFromCityFile('cities-usa.tsv')
