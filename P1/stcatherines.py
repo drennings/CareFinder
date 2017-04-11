67290065e5d7e59196365db36b21c8acdfcc6b77
Daan Rennigns <daanrennings@hotmail.com>
2017-04-11 21:10:18 +0200

    Added stcatherine data to current dataset

--- P1/stcatherines.py
+++ P1/stcatherines.py
@@ -7,31 +7,40 @@ def mapNameToLocation(name, locations):
     return hospitalstats.mostSimilar(hospitalstats.levenshteinSimilarity(name, locations))
 
 def mapNamesToLocation(path, threshold):
-    print('test')
-    tsvout = open('stcatherines.tsv', 'w')
-    #tsvout.write('name' + '\t' + 'speciality' + '\t' + 'hospital' + '\t' + 'mapped' + '\t' + 'ratio' + '\t' + 'lat' + '\t' + 'lon' + '\t' + 'url'+ '\t' + 'doctorsLocations' +'\n')
-    locations = (hospitalstats.getLocations('ny', False))
+    import pandas as pd
+    doctors = pd.read_excel('current_data.xlsx')
+    
+    locations = (hospitalstats.getLocations('ny', False, 'allHospitalDetails_in_NY-filtered.tsv'))
     keys = [ k for k in locations ]
     with open(path,'rt') as csvin:
         csvin = csv.reader(csvin)
+        i = 6053;
         for row in csvin:
-            try:
-                name = row[0]
-                speciality = row[1]
-                hospital = row[2]
-                url = row[4]
-                doctorsLocation = row[3]
-                (mapped, ratio) = mapNameToLocation(hospital, keys)
+            name = row[0]
+            speciality = row[1]
+            hospital = str(row[2]).split(",")
+            url = row[4]
+            doctorsLocation = row[3]
+            for workingplace in hospital:
+                (mapped, ratio) = mapNameToLocation(workingplace, keys)
                 (lat, lon) = locations[mapped]
                 if ratio > threshold:
-                    #print hospital + ': ' + str(mapped) + ', ' + str(ratio)
-                    tsvout.write(name + '\t' + speciality + '\t' + hospital + '\t' + mapped + '\t' + str(ratio) + '\t' + str(lat) + '\t' + str(lon) + '\t' + url+ '\t' + doctorsLocation +'\n')
-                    #print(name + '\t' + speciality + '\t' + hospital + '\t' + mapped + '\t' + str(ratio) + '\t' + str(lat) + '\t' + str(lon)+ + '\t' + str(url)+ '\t' + str(doctorsLocation), file=tsvout)
-            except:
-                raise
-
+                    doctors.loc[i,'Name'] = name
+                    doctors.loc[i,'Spec'] = speciality
+                    doctors.loc[i,'Hosp'] = workingplace
+                    doctors.loc[i,'lat'] = lat
+                    doctors.loc[i,'lon'] = lon
+                    i = i + 1;
+                    #print( str(i) + str(name) + str(speciality) + str(hospital) + str(lat) + str(lon) + str(ratio))
+    writer = pd.ExcelWriter('current_data.xlsx', engine='xlsxwriter')
+    doctors.to_excel(writer, sheet_name='Sheet1')
+    writer.save()
+    print(doctors)
+    return
+    
+                
 def main():
     #crawl() # done
-    mapNamesToLocation('stcatherines.csv', -0.10)
+    mapNamesToLocation('stcatherines.csv', 0.9)
 
 main()
